46c67f56d27e981eb24a40721b2be72688f7709b
interglobalmedia <interglobalmedia@gmail.com>
2018-11-22 20:58:50 -0500

    Updates

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.94e832fc94052c5c95c5b3ad0ac58638.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.a8ae805812b5040fe3b3e982c30eaf31.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
