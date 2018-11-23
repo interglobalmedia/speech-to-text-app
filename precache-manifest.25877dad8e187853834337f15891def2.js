28b489ee68afcb742b654d2ae3b778d0c79aa45b
interglobalmedia <interglobalmedia@gmail.com>
2018-11-23 06:51:30 -0500

    Updates

---
+++ precache-manifest.25877dad8e187853834337f15891def2.js
@@ -0,0 +1,40 @@
+self.__precacheManifest = [
+  {
+    "url": "styles/main.690dfc9a9eacd0ee4d0f.css"
+  },
+  {
+    "url": "styles/Speech.690dfc9a9eacd0ee4d0f.css"
+  },
+  {
+    "revision": "2cc0ddfdc87f52e9d8c0c7d5b6efbebe",
+    "url": "src/favicon.2cc0ddfd.ico"
+  },
+  {
+    "revision": "c00558e3eb099318bfd7",
+    "url": "scripts/vendors~main.1ad1cd98b3cea083930b.chunk.js"
+  },
+  {
+    "revision": "91502a5c5a80b43265a1",
+    "url": "scripts/vendors~Speech.f0e3c9ecdeb61f482646.chunk.js"
+  },
+  {
+    "revision": "526080fd283e0b39d519",
+    "url": "scripts/runtime.f6f71c9f7a8cf14b349f.chunk.js"
+  },
+  {
+    "revision": "1365bbd7f1fd166121d6",
+    "url": "scripts/main.f8d88775b8bc47d47855.chunk.js"
+  },
+  {
+    "revision": "07fd12356b7d18233857",
+    "url": "scripts/Speech.d477efae3d7e20cf3ae3.chunk.js"
+  },
+  {
+    "revision": "23378fffa3b2d116a81dbb2052c30463",
+    "url": "index.html"
+  },
+  {
+    "revision": "2cc0ddfdc87f52e9d8c0c7d5b6efbebe",
+    "url": "favicon.ico"
+  }
+];
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.baf29cc949ed756211365419cad2df3c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.bcc470e9eca699aa64eaf353e20e81fe.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
