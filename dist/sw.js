4f753f806e8169735cdf2e872e1ea20d360d587c
interglobalmedia <interglobalmedia@gmail.com>
2018-11-22 20:57:41 -0500

    Add new build

--- dist/sw.js
+++ dist/sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.94e832fc94052c5c95c5b3ad0ac58638.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.3f046bd25b5c5ea7395604c5aec30b72.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
