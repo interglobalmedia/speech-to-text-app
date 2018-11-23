bdafd665776942a664ddc092907a3b3b8beaaaba
interglobalmedia <interglobalmedia@gmail.com>
2018-11-22 21:47:41 -0500

    Add new build with magic comment

---
+++ dist/precache-manifest.c57045964f1b61368486c1892f6c4a00.js
@@ -0,0 +1,32 @@
+self.__precacheManifest = [
+  {
+    "url": "styles/main.a09a7962988743a0534b.css"
+  },
+  {
+    "revision": "2cc0ddfdc87f52e9d8c0c7d5b6efbebe",
+    "url": "src/favicon.2cc0ddfd.ico"
+  },
+  {
+    "url": "scripts/vendors~vendor.51776cd6a0ac09936326.chunk.js"
+  },
+  {
+    "url": "scripts/vendors~main~vendor.b5f8c6a94d2139ed18ec.chunk.js"
+  },
+  {
+    "url": "scripts/vendor.8dccc47a7ec10113468e.chunk.js"
+  },
+  {
+    "url": "scripts/runtime.2efb35bd0614e4fbadb0.chunk.js"
+  },
+  {
+    "url": "scripts/main.0ce8ae02680441a4b1f0.chunk.js"
+  },
+  {
+    "revision": "bd679550f6a875f9f24edc8f3f12e3cd",
+    "url": "index.html"
+  },
+  {
+    "revision": "2cc0ddfdc87f52e9d8c0c7d5b6efbebe",
+    "url": "favicon.ico"
+  }
+];
