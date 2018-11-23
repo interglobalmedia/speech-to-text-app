76fe3beb223b9c6cce6a3f1371e544106c4e9953
interglobalmedia <interglobalmedia@gmail.com>
2018-11-22 22:57:29 -0500

    Add new deployment files

---
+++ dist/precache-manifest.5482de4e91238e80e43c20e77b080d59.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = [
+  {
+    "url": "styles/main.eed63a89b1c3427f6885.css"
+  },
+  {
+    "revision": "2cc0ddfdc87f52e9d8c0c7d5b6efbebe",
+    "url": "src/favicon.2cc0ddfd.ico"
+  },
+  {
+    "revision": "ad2786838252c6dca2dc",
+    "url": "scripts/vendors~main.7bfaf4aa0dfa75093ffa.chunk.js"
+  },
+  {
+    "revision": "cd644c8a118b28733942",
+    "url": "scripts/runtime.ef11508e2dbc2e05c864.chunk.js"
+  },
+  {
+    "revision": "8b2669ee67d8437428b5",
+    "url": "scripts/main.12984d89d9c4b4608e9f.chunk.js"
+  },
+  {
+    "revision": "4162d209beb5e4e0db6d14011f863cbb",
+    "url": "index.html"
+  },
+  {
+    "revision": "2cc0ddfdc87f52e9d8c0c7d5b6efbebe",
+    "url": "favicon.ico"
+  }
+];
--- dist/sw.js
+++ dist/sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.baf29cc949ed756211365419cad2df3c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.5482de4e91238e80e43c20e77b080d59.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
