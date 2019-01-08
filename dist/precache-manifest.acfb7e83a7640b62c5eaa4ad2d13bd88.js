fee99c23c6fd7401a39112b46ad3e9d8e70282f3
interglobalmedia <interglobalmedia@gmail.com>
2019-01-08 11:50:59 -0500

    Add new build

---
+++ dist/precache-manifest.acfb7e83a7640b62c5eaa4ad2d13bd88.js
@@ -0,0 +1,36 @@
+self.__precacheManifest = [
+  {
+    "revision": "b27413fde9be90449362",
+    "url": "scripts/vendors~Speech.e5d54fc419de85b1328d.chunk.js"
+  },
+  {
+    "url": "styles/Speech.f34911fc172f9dd14743.css"
+  },
+  {
+    "revision": "7234309614dbfaf03e72",
+    "url": "scripts/Speech.8dc1e1d9baf35f97b808.chunk.js"
+  },
+  {
+    "revision": "038d63b2000b6f90a0e8",
+    "url": "scripts/runtime.e1a78e032a4fc6640051.chunk.js"
+  },
+  {
+    "revision": "c00558e3eb099318bfd7",
+    "url": "scripts/vendors~main.1ad1cd98b3cea083930b.chunk.js"
+  },
+  {
+    "url": "styles/main.f34911fc172f9dd14743.css"
+  },
+  {
+    "revision": "b7ddf38e4de3bbf2cff5",
+    "url": "scripts/main.537dc228110f284106e0.chunk.js"
+  },
+  {
+    "revision": "f7761add6c97da1c481ef33dacd71e62",
+    "url": "index.html"
+  },
+  {
+    "revision": "2cc0ddfdc87f52e9d8c0c7d5b6efbebe",
+    "url": "favicon.ico"
+  }
+];
